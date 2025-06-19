cac8b85acfc7ffc6711f8d337c637dde1a3b1e52
Andre <argregoletto@gmail.com>
2025-06-19 15:32:51 -0300

    create layout default

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -3,5 +3,13 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    // return view('welcome');
+    return view('teste');
 });
+
+Route::get('/dash', function () {
+    // return view('welcome');
+    return view('pages.dashboard');
+});
+
+require __DIR__.'/api.php';
